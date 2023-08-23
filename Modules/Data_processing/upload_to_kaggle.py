94bb4d594741fc9dc504f16aa50984a0cd64d053
Kosuketf2 <142601619+Kosuketf2@users.noreply.github.com>
2023-08-23 23:30:57 +0100

    clean up

--- Modules/Data_processing/upload_to_kaggle.py
+++ Modules/Data_processing/upload_to_kaggle.py
@@ -2,7 +2,7 @@ import os
 
 # Directory containing your CSV files
 csv_directory = r""  # Update with the directory path
-dataset_slug = "https://github.com/Kosuketf2/HighlanderPugStatsEU"  # Replace with your dataset slug
+dataset_slug = "kosuketf2/hlpugstats"  # Replace with your dataset slug
 
 # Get a list of CSV files in the directory
 csv_files = [os.path.join(csv_directory, f) for f in os.listdir(csv_directory) if f.endswith(".csv")]
